cabe8f5149c072aff2d6d60775842ff50be03bd8
mercy <913037829@qq.com>
2023-11-20 13:05:34 +0800

    123

--- FourArithmetic/src/FourArithmetic.java
+++ FourArithmetic/src/FourArithmetic.java
@@ -76,7 +76,6 @@ public class FourArithmetic {
         else
             return false;
     }
-
     //获取操作符的优先级
     public static int priority(String op){
         if(op.equals("*") || op.equals("/")){
@@ -96,8 +95,17 @@ public class FourArithmetic {
             char ch = expression.charAt(index);
             if(ch!=46 && (ch <= 47 || ch >= 58)){
                 //是操作符，直接添加至list中
-                index ++ ;
-                list.add(ch+"");
+                if(ch == '-' && (index - 1 < 0 || !isNumber(expression.charAt(index - 1) + "")))
+                {
+                    index++;
+                    list.add("0");
+                    list.add(ch + "");
+                }
+                else
+                {
+                    index++;
+                    list.add(ch + "");
+                }
             }else{
                 //是数字,判断多位数的情况
                 String str = "";
@@ -113,7 +121,7 @@ public class FourArithmetic {
     }
 
     //根据后缀表达式list计算结果
-    private static BigDecimal calculate(List<String> list) {
+    private static BigDecimal cal(List<String> list) {
         Stack<BigDecimal> stack = new Stack<>();
         for(int i=0; i<list.size(); i++){
             String item = list.get(i);
@@ -165,44 +173,67 @@ public class FourArithmetic {
         }
         return stack.pop();
     }
-    public static BigDecimal caculate(String expression)
+    public static BigDecimal caculatePlain(String s1, String item, String s2)
+    {
+        BigDecimal num1 = new BigDecimal(s1);
+        BigDecimal num2 = new BigDecimal(s2);
+        BigDecimal res;
+        if(item.equals("+"))
+            res = num1.add(num2);
+        else if(item.equals("-"))
+            res = num1.subtract(num2);
+        else if(item.equals("*"))
+            res = num1.multiply(num2);
+        else if(item.equals("/"))
+        {
+            try {
+                res = num1.divide(num2);
+            }catch (ArithmeticException a)
+            {
+                try {
+                    res = num1.divide(num2, 10, RoundingMode.HALF_UP);
+                }
+                catch (ArithmeticException b)
+                {
+                    System.out.println("运算错误！请重新输入：");
+                    return null;
+                }
+            }
+        }
+        else
+        {
+            System.out.println("运算符号错误！请重新输入:");
+            return null;
+        }
+        return res.stripTrailingZeros();
+    }
+    public static BigDecimal calculateEnginner(String expression)
     {
         if(expression.isBlank())
             return null;
         List<String> expressionList = expressionToList(expression.replace(" ", ""));
-        //System.out.println("中缀表达式转为list结构="+expressionList);
+//        System.out.println("中缀表达式转为list结构="+expressionList);
         //将中缀表达式转换为后缀表达式
         List<String> suffixList = parseToSuffixExpression(expressionList);
         //System.out.println("对应的后缀表达式列表结构="+suffixList);
         //根据后缀表达式计算结果
         if(suffixList != null)
         {
-            BigDecimal calculateResult = calculate(suffixList);
+            BigDecimal calculateResult = cal(suffixList);
             if(calculateResult != null)
                 return calculateResult;
         }
         return null;
     }
-//    public static void main(String []args){
-//        System.out.println("请输入运算式：");
-//        Scanner in = new Scanner(System.in);
-//        while(true)
-//        {
-//            String expression = in.nextLine();
-//            if(expression.isBlank())
-//                continue;
-//            List<String> expressionList = expressionToList(expression.replace(" ", ""));
-//            //System.out.println("中缀表达式转为list结构="+expressionList);
-//            //将中缀表达式转换为后缀表达式
-//            List<String> suffixList = parseToSuffixExpression(expressionList);
-//            //System.out.println("对应的后缀表达式列表结构="+suffixList);
-//            //根据后缀表达式计算结果
-//            if(suffixList != null)
-//            {
-//                BigDecimal calculateResult = calculate(suffixList);
-//                if(calculateResult != null)
-//                    System.out.println("= " + calculateResult);
-//            }
-//        }
-//    }
+    public static void main(String []args){
+        System.out.println("请输入运算式：");
+        Scanner in = new Scanner(System.in);
+        while(true)
+        {
+            String s1 = in.nextLine();
+            String s2 = in.nextLine();
+            String s3 = in.nextLine();
+            System.out.println(caculatePlain(s1, s2, s3));
+        }
+    }
 }
