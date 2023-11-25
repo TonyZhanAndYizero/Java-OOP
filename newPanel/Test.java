effaeacace15788f65aedfc93a654644914de621
mercy <913037829@qq.com>
2023-11-25 20:53:30 +0800

    角度转换器

---
+++ newPanel/Test.java
@@ -0,0 +1,108 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+/*
+ * Created by JFormDesigner on Sat Nov 25 16:02:13 CST 2023
+ */
+
+
+
+/**
+ * @author HeMercy
+ */
+public class Test extends JFrame {
+    public Test() {
+        initComponents();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        dialogPane = new JPanel();
+        contentPanel = new JPanel();
+        panel1 = new Angle();
+
+        //======== this ========
+        setMinimumSize(new Dimension(398, 585));
+        var contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        //======== dialogPane ========
+        {
+            dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+            dialogPane.setLayout(null);
+
+            //======== contentPanel ========
+            {
+                contentPanel.setLayout(null);
+
+                {
+                    // compute preferred size
+                    Dimension preferredSize = new Dimension();
+                    for(int i = 0; i < contentPanel.getComponentCount(); i++) {
+                        Rectangle bounds = contentPanel.getComponent(i).getBounds();
+                        preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                        preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+                    }
+                    Insets insets = contentPanel.getInsets();
+                    preferredSize.width += insets.right;
+                    preferredSize.height += insets.bottom;
+                    contentPanel.setMinimumSize(preferredSize);
+                    contentPanel.setPreferredSize(preferredSize);
+                }
+            }
+            dialogPane.add(contentPanel);
+            contentPanel.setBounds(12, 12, 374, contentPanel.getPreferredSize().height);
+
+            //---- panel1 ----
+            panel1.setMinimumSize(new Dimension(300, 100));
+            dialogPane.add(panel1);
+            panel1.setBounds(0, -5, 398, 570);
+
+            {
+                // compute preferred size
+                Dimension preferredSize = new Dimension();
+                for(int i = 0; i < dialogPane.getComponentCount(); i++) {
+                    Rectangle bounds = dialogPane.getComponent(i).getBounds();
+                    preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                    preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+                }
+                Insets insets = dialogPane.getInsets();
+                preferredSize.width += insets.right;
+                preferredSize.height += insets.bottom;
+                dialogPane.setMinimumSize(preferredSize);
+                dialogPane.setPreferredSize(preferredSize);
+            }
+        }
+        contentPane.add(dialogPane);
+        dialogPane.setBounds(0, 0, 398, 560);
+
+        {
+            // compute preferred size
+            Dimension preferredSize = new Dimension();
+            for(int i = 0; i < contentPane.getComponentCount(); i++) {
+                Rectangle bounds = contentPane.getComponent(i).getBounds();
+                preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+            }
+            Insets insets = contentPane.getInsets();
+            preferredSize.width += insets.right;
+            preferredSize.height += insets.bottom;
+            contentPane.setMinimumSize(preferredSize);
+            contentPane.setPreferredSize(preferredSize);
+        }
+        setSize(400, 595);
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    private JPanel dialogPane;
+    private JPanel contentPanel;
+    private Angle panel1;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+    public static void main(String[] args) {
+        new Test().setVisible(true);
+    }
+}
