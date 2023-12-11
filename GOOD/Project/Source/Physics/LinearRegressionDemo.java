cd117086acf1bbb82fe26e511b623e307b7a9b55
TonyZhanAndYizero <2860021044@qq.com>
2023-12-11 21:36:16 +0800

    画图更新

--- GOOD/Project/Source/Physics/LinearRegressionDemo.java
+++ GOOD/Project/Source/Physics/LinearRegressionDemo.java
@@ -73,18 +73,113 @@ public class LinearRegressionDemo extends JFrame {
     }
 
     private void saveImage(File file) {
-        BufferedImage image;
+        BufferedImage image = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
+        // 从BufferedImage对象中获取一个Graphics2D对象
+        Graphics2D g = image.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setBackground(Color.WHITE);
+        g.clearRect(0, 0, 1000, 1000);
+        draw(g,drawPanel);
+        // 创建一个File对象，指定要保存的文件名和路径
         try {
-            //使用Robot类抓取指定区域的屏幕截图
-            image = new Robot().createScreenCapture(
-                    new Rectangle(this.getX(), this.getY(), this.getWidth(), this.getHeight()));
-            //使用ImageIO类保存图片
+            // 使用ImageIO类的write方法，将BufferedImage对象保存为png文件
             ImageIO.write(image, "png", file);
-            //提示保存成功
-            JOptionPane.showMessageDialog(null, "Successfully saved!");
-        } catch (AWTException | IOException e) {
+        } catch (IOException e) {
+            // 处理异常
             e.printStackTrace();
         }
+    }
+
+    private void draw(Graphics g,JPanel jPanel) {
+        // 设置背景颜色为白色
+        jPanel.setBackground(Color.WHITE);
+
+        BasicStroke stokeLine = new BasicStroke(0.5f);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(stokeLine);
+        g.setColor(Color.LIGHT_GRAY);
+        for (int i = 1; i < 21; i++) {
+            g.drawLine(150 + i * 35, 900, 150 + i * 35, 50);
+            g.drawLine(150, 600 - i * 25, 900, 600 - i * 25);
+            if (i <= 10) {
+                g.drawLine(150, 600 + i * 25, 900, 600 + i * 25);
+            }
+        }
+
+        stokeLine = new BasicStroke(2.0f);
+        g2.setStroke(stokeLine);
+        // 设置画笔颜色为黑色
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        // 绘制坐标轴
+        g.drawLine(150, 50, 150, 900);//y
+        g.drawLine(150, 600, 900, 600);//x
+        // 绘制坐标轴的箭头
+        g.drawLine(150, 50, 145, 60);
+        g.drawLine(150, 50, 155, 60);
+        g.drawLine(900, 600, 890, 595);
+        g.drawLine(900, 600, 890, 605);
+
+
+        BigDecimal xEnd = getMax(linearRegression.XNums).multiply(new BigDecimal("1.1")).setScale(0, RoundingMode.CEILING),
+                yEnd = getMax(linearRegression.YNums).multiply(new BigDecimal("1.1")).setScale(0, RoundingMode.CEILING);
+        BigDecimal xDis = xEnd.divide(new BigDecimal(10), 2, RoundingMode.HALF_UP),
+                yDis = yEnd.divide(new BigDecimal(10), 2, RoundingMode.HALF_UP);
+        BigDecimal temp;
+        // 绘制坐标轴的刻度和标签
+        for (int i = 0; i < 11; i++) {
+            temp = new BigDecimal(i);
+            // x轴的刻度和标签
+            if (i != 0) {
+                g.drawLine(150 + i * 70, 600, 150 + i * 70, 605);
+                g.drawString(temp.multiply(xDis).toPlainString(), 130 + i * 70, 625);
+            }// y轴的刻度和标签
+            g.drawLine(150, 600 - i * 50, 145, 600 - i * 50);
+            g.drawString(temp.multiply(yDis).toPlainString(), 75, 605 - i * 50);
+            if (i > 0 && i <= 5) {
+                g.drawLine(150, 600 + i * 50, 145, 600 + i * 50);
+                g.drawString(temp.multiply(yDis).multiply(new BigDecimal(-1)).toPlainString(), 68, 605 + i * 50);
+            }
+        }
+        // 绘制坐标轴的名称
+        g.drawString("x", 910, 615);
+        g.drawString("y", 135, 45);
+
+
+        int n = linearRegression.XNums.size();
+        BigDecimal xPixel = new BigDecimal(700).divide(xEnd, 5, RoundingMode.HALF_UP),
+                yPixel = new BigDecimal(500).divide(yEnd, 5, RoundingMode.HALF_UP);
+
+        // 绘制数据点
+        g.setColor(Color.RED); // 设置画笔颜色为红色
+        for (int i = 0; i < n; i++) {
+            // 将数据点转换为像素点
+            int px = 150 + (int) Double.parseDouble(linearRegression.XNums.get(i).multiply(xPixel).toPlainString());
+            int py = 600 - (int) Double.parseDouble(linearRegression.YNums.get(i).multiply(yPixel).toPlainString());
+            // 绘制一个半径为4的圆点
+            g.fillOval(px - 4, py - 4, 8, 8);
+        }
+        // 绘制回归线
+        stokeLine = new BasicStroke(1.5f);
+        g2.setStroke(stokeLine);
+        g.setColor(Color.BLUE); // 设置画笔颜色为蓝色
+        // 计算回归线的起点和终点的像素坐标
+        BigDecimal startY = linearRegression.a,
+                endY = linearRegression.a.add(linearRegression.b.multiply(xEnd));
+        int x1 = 150;
+        int y1 = 600 - (int) Double.parseDouble(startY.multiply(yPixel).toPlainString());
+        int x2 = 850;
+        int y2 = 600 - (int) Double.parseDouble(endY.multiply(yPixel).toPlainString());
+        // 绘制一条直线
+        g.drawLine(x1, y1, x2, y2);
+        // 绘制回归方程
+        g.setColor(Color.BLACK); // 设置画笔颜色为黑色
+        // 格式化方程中的小数
+        String sb = String.format("%.2f", linearRegression.b);
+        String sa = String.format("%.2f", linearRegression.a);
+        // 绘制方程的字符串
+        g.setFont(new Font("Times New Roman", Font.BOLD, 24));
+        g.drawString("y = " + sb + "x + " + sa, 200, 80);
     }
 
     /**
@@ -93,103 +188,11 @@ public class LinearRegressionDemo extends JFrame {
      * @author TonyZhan
      */
     class DrawPanel extends JPanel {
-        private void draw(Graphics g) {
-            BasicStroke stokeLine = new BasicStroke(0.5f);
-            Graphics2D g2 = (Graphics2D) g;
-            g2.setStroke(stokeLine);
-            g.setColor(Color.LIGHT_GRAY);
-            for (int i = 1; i < 21; i++) {
-                g.drawLine(150 + i * 35, 900, 150 + i * 35, 50);
-                g.drawLine(150, 600 - i * 25, 900, 600 - i * 25);
-                if (i <= 10) {
-                    g.drawLine(150, 600 + i * 25, 900, 600 + i * 25);
-                }
-            }
-
-            stokeLine = new BasicStroke(2.0f);
-            g2.setStroke(stokeLine);
-
-            // 设置背景颜色为白色
-            setBackground(Color.WHITE);
-            // 设置画笔颜色为黑色
-            g.setColor(Color.BLACK);
-            g.setFont(new Font("Times New Roman", Font.BOLD, 18));
-            // 绘制坐标轴
-            g.drawLine(150, 50, 150, 900);//y
-            g.drawLine(150, 600, 900, 600);//x
-            // 绘制坐标轴的箭头
-            g.drawLine(150, 50, 145, 60);
-            g.drawLine(150, 50, 155, 60);
-            g.drawLine(900, 600, 890, 595);
-            g.drawLine(900, 600, 890, 605);
-
-
-            BigDecimal xEnd = getMax(linearRegression.XNums).multiply(new BigDecimal("1.1")).setScale(0, RoundingMode.CEILING),
-                    yEnd = getMax(linearRegression.YNums).multiply(new BigDecimal("1.1")).setScale(0, RoundingMode.CEILING);
-            BigDecimal xDis = xEnd.divide(new BigDecimal(10), 2, RoundingMode.HALF_UP),
-                    yDis = yEnd.divide(new BigDecimal(10), 2, RoundingMode.HALF_UP);
-            BigDecimal temp;
-            // 绘制坐标轴的刻度和标签
-            for (int i = 0; i < 11; i++) {
-                temp = new BigDecimal(i);
-                // x轴的刻度和标签
-                if (i != 0) {
-                    g.drawLine(150 + i * 70, 600, 150 + i * 70, 605);
-                    g.drawString(temp.multiply(xDis).toPlainString(), 130 + i * 70, 625);
-                }// y轴的刻度和标签
-                g.drawLine(150, 600 - i * 50, 145, 600 - i * 50);
-                g.drawString(temp.multiply(yDis).toPlainString(), 75, 605 - i * 50);
-                if (i > 0 && i <= 5) {
-                    g.drawLine(150, 600 + i * 50, 145, 600 + i * 50);
-                    g.drawString(temp.multiply(yDis).multiply(new BigDecimal(-1)).toPlainString(), 68, 605 + i * 50);
-                }
-            }
-            // 绘制坐标轴的名称
-            g.drawString("x", 910, 615);
-            g.drawString("y", 135, 45);
-
-
-            int n = linearRegression.XNums.size();
-            BigDecimal xPixel = new BigDecimal(700).divide(xEnd, 5, RoundingMode.HALF_UP),
-                    yPixel = new BigDecimal(500).divide(yEnd, 5, RoundingMode.HALF_UP);
-
-            // 绘制数据点
-            g.setColor(Color.RED); // 设置画笔颜色为红色
-            for (int i = 0; i < n; i++) {
-                // 将数据点转换为像素点
-                int px = 150 + (int) Double.parseDouble(linearRegression.XNums.get(i).multiply(xPixel).toPlainString());
-                int py = 600 - (int) Double.parseDouble(linearRegression.YNums.get(i).multiply(yPixel).toPlainString());
-                // 绘制一个半径为4的圆点
-                g.fillOval(px - 4, py - 4, 8, 8);
-            }
-            // 绘制回归线
-            stokeLine = new BasicStroke(1.5f);
-            g2.setStroke(stokeLine);
-            g.setColor(Color.BLUE); // 设置画笔颜色为蓝色
-            // 计算回归线的起点和终点的像素坐标
-            BigDecimal startY = linearRegression.a,
-                    endY = linearRegression.a.add(linearRegression.b.multiply(xEnd));
-            int x1 = 150;
-            int y1 = 600 - (int) Double.parseDouble(startY.multiply(yPixel).toPlainString());
-            int x2 = 850;
-            int y2 = 600 - (int) Double.parseDouble(endY.multiply(yPixel).toPlainString());
-            // 绘制一条直线
-            g.drawLine(x1, y1, x2, y2);
-            // 绘制回归方程
-            g.setColor(Color.BLACK); // 设置画笔颜色为黑色
-            // 格式化方程中的小数
-            String sb = String.format("%.2f", linearRegression.b);
-            String sa = String.format("%.2f", linearRegression.a);
-            // 绘制方程的字符串
-            g.setFont(new Font("Times New Roman", Font.BOLD, 24));
-            g.drawString("y = " + sb + "x + " + sa, 200, 80);
-        }
-
         // 重写paintComponent方法，实现绘图逻辑
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            draw(g);
+            draw(g,this);
         }
     }
 
